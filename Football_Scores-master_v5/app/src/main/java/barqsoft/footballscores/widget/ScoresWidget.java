10116516144fc9f94bccfa82ce78c3943d5a2b57
colinyeh <colin.yeh@gmail.com>
2016-02-17 21:22:14 +0800

    Project3-SuperDuo

---
+++ Football_Scores-master_v5/app/src/main/java/barqsoft/footballscores/widget/ScoresWidget.java
@@ -0,0 +1,36 @@
+package barqsoft.footballscores.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import barqsoft.footballscores.service.myFetchService;
+
+/**
+ * Created by User on 2016/2/16.
+ */
+
+
+public class ScoresWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        context.startService(new Intent(context, ScoresWidgetIntentService.class));
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        context.startService(new Intent(context, ScoresWidgetIntentService.class));
+    }
+
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+        if (myFetchService.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            context.startService(new Intent(context, ScoresWidgetIntentService.class));
+        }
+    }
+}
